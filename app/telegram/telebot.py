25243a3d3ab4be7bf61ee8458d9f3feb9bee1287
Antonio Calì <antoniodavidecali@outlook.com>
2020-08-23 11:22:45 +0200

    Config in Yaml

--- app/telegram/telebot.py
+++ app/telegram/telebot.py
@@ -1,28 +1,24 @@
 from telethon import TelegramClient
-from telethon.hints import EntityLike
-from telethon.tl.types import PeerUser
+import yaml
 
-# Use your own values from my.telegram.org
-api_id = 1733178
-api_hash = "cdb7ddc714337bc3124a5d3be496305d"
-bot_token = "1359800920:AAEdWoszwb3MVlmag9400flUG1DTItXhyHw"
+# Use a config file for moment
+with open('./app/telegram/config.yaml', 'r') as stream: 
+    config = yaml.safe_load(stream)
+    api_id = config['api_id']
+    api_hash = config['api_hash']
+    bot_token = config['bot_token']
 
-# We have to manually call "start" if we want an explicit bot token
 
 bot = TelegramClient("bot", api_id, api_hash)
-from telethon.tl.functions.messages import AddChatUserRequest
 # Starting as a bot account
 
 
 # But then we can use the client instance as usual
 async def main():
-    username = await bot.get_input_entity('t.me/provalolasd')
-    print(username)
-    # await bot(AddChatUserRequest())
-    message = await bot.send_message(username, message="Hello")
-
-    # message = bot.send_message(username, message='Some <b>bold</b> and <i>italic</i> text')
-    # print(message)
+    channel = await bot.get_input_entity('t.me/provalolasd')
+    print(channel)
+    message = await bot.send_message(channel, message="Hello")
+    print(message)
 
 
 with bot.start(bot_token=bot_token):
