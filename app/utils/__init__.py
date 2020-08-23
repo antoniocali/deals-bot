1eeb87b6a6f6eb147400371d2fa631ae2f9a6adb
Antonio Calì <antoniodavidecali@outlook.com>
2020-08-23 13:47:13 +0200

    Shorter url in place

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,3 +1,18 @@
+import asyncio
+import requests
+from app.utils.config import Config
+
+
 def removeSpecialFromPrice(textInput: str) -> str:
     return textInput.replace(".", "").replace(",", ".").replace("€", "")
 
+
+async def shortenUrl(url: str) -> dict:
+    url = "https://api.shorte.st/v1/data/url"
+    token = Config.get_instance().shortest_token
+    headers = {"public-api-token": token}
+    data = {"urlToShorten": url}
+    response = requests.put(url, data=data, headers=headers)
+    if response.ok:
+        print(response.json())
+        return response.json()
---
+++ app/utils/config.py
@@ -0,0 +1,28 @@
+import yaml
+
+
+class Config:
+    __instance__ = None
+
+    def __init__(self):
+        """ Constructor.
+       """
+        if Config.__instance__ is None:
+            Config.__instance__ = self
+            with open("./app/telegram/config.yaml", "r") as stream:
+                config = yaml.safe_load(stream)
+                self.api_id = config["api_id"]
+                self.api_hash = config["api_hash"]
+                self.bot_token = config["bot_token"]
+                self.shortest_token = config["shortest_token"]
+        else:
+            raise Exception("You cannot create another SingletonGovt class")
+
+    @staticmethod
+    def get_instance():
+        """ Static method to fetch the current instance.
+       """
+        if not Config.__instance__:
+            Config()
+        return Config.__instance__
+
